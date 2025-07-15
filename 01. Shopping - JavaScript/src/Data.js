fdde760a3cc7594a3a6ae9f7a977f9b367d5b7fa
jmisrikhanov <jmisrikhanov@gmail.com>
2025-07-15 22:17:23 +0200

    Data moved to new js file, and imported

---
+++ 01. Shopping - JavaScript/src/Data.js
@@ -0,0 +1,16 @@
+let shopItemsData = [
+  {
+    id: "sdfdsf",
+    name: "casual shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "sdfdsf32",
+    name: "casual shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+];
--- 01. Shopping - JavaScript/src/main.js
+++ 01. Shopping - JavaScript/src/main.js
@@ -1,22 +1,5 @@
 let shop = document.getElementById("shop");
 
-let shopItemsData = [
-  {
-    id: "sdfdsf",
-    name: "casual shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-2.jpg",
-  },
-  {
-    id: "sdfdsf32",
-    name: "casual shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-2.jpg",
-  },
-];
-
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
