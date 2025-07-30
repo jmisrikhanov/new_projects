98f9900a8c3227812579b03faa0ee331e1643c62
jmisrikhanov <jmisrikhanov@gmail.com>
2025-07-30 23:11:03 +0200

    NewEventPage set up

---
+++ 02. Calendar Scheduler App - Full Stack Next.js, React, Typescript, Tailwind/calendar-scheduler-app/app/(main)/(private)/events/new/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function NewEventPage() {
+  return (
+    // Container Card component centered on the page with a max width
+    <Card className="max-w-md mx-auto border-8 border-blue-200 shadow-2xl shadow-accent-foreground">
+      <CardHeader>
+        <CardTitle>New Event</CardTitle>
+      </CardHeader>
+
+      <CardContent>{/* <EventForm /> */}</CardContent>
+    </Card>
+  );
+}
